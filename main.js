87a726031a5f635c26fbd26d78812aa443280083
Mequidi <nikhilsejwal234@gmail.com>
2021-06-02 19:14:08 +0530

    all buttons made

---
+++ main.js
@@ -0,0 +1,41 @@
+const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("save-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
+const savedUrlBtn = document.getElementById("saved-url-btn");
+
+let url = [];
+
+function render(){ 
+    console.log("saved url button clicked");
+    let listEl = "";
+    for (let i = 0; i < url.length; i++) {
+        listEl += `<li>
+            <a href="${url[i]}"> 
+                ${url[i]}
+            </a>
+        </li>`; 
+    }
+    ulEl.innerHTML = listEl;
+}
+
+function storeUrl()
+{
+    localStorage.setItem("url",JSON.stringify(url));
+}
+
+saveBtn.addEventListener("click", function () {
+    console.log("save button clicked!");
+    url.push(inputEl.value);
+})
+
+tabBtn.addEventListener("click", function () {
+    console.log("tab button clicked!");
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+    console.log("deleted button double clicked!");
+})
+
+savedUrlBtn.addEventListener("click", render);
